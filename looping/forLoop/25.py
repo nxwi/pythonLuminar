007f13ccb162dd17a803d29da086d7c692d6d4b9
Naswih <51323070+nxwi@users.noreply.github.com>
2023-09-29 16:18:16 +0530

    update

---
+++ looping/forLoop/25.py
@@ -0,0 +1,6 @@
+# take range as user input and print "Hai"
+
+x = int(input("Enter the range:"))
+
+for i in range(x):
+    print("Hai")
---
+++ looping/forLoop/26.py
@@ -0,0 +1,4 @@
+# print first 10 natural numbers
+
+for i in range(10):
+    print(i)

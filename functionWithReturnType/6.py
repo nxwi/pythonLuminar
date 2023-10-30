8b381ca76aacc6bfa288f4270ed9f57c1e4ae9a8
Naswih <51323070+nxwi@users.noreply.github.com>
2023-10-30 19:46:45 +0530

    Update 6.py

--- functionWithReturnType/6.py
+++ functionWithReturnType/6.py
@@ -1,2 +1,13 @@
 # Solve quadratic equation using funtion with multiple return
 
+def quad(a, b, c):
+    x1 = (-b - (b**2 - 4 * a * c) ** 0.5) / 2 * a
+    x2 = (-b - (b**2 - 4 * a * c) ** 0.5) / 2 * a
+    return '%d %d' % (x1, x2)
+
+
+x = int(input("Enter x:"))
+y = int(input("Enter y:"))
+z = int(input("Enter z:"))
+
+print(quad(x, y, z))

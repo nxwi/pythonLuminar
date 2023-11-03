40700ea5d2fd35e4941d56ee705326ec48350710
Naswih <51323070+nxwi@users.noreply.github.com>
2023-11-03 16:20:14 +0530

    Create mathModule.py

---
+++ modules/biultIn/mathModule.py
@@ -0,0 +1,13 @@
+import math
+
+print(math.factorial(5))
+print(math.sqrt(16))
+print(math.sin(1))
+print(math.pow(2, 4))
+print(math.floor(11.9))
+print(math.ceil(3.1))
+
+help(math)
+
+r = int(input("Enter the radius:"))
+print(math.pi * math.pow(r, 2))

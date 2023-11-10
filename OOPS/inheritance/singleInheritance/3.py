8cbbcc9e02d0584daf200f91f7f7eb3512a79a62
Naswih <51323070+nxwi@users.noreply.github.com>
2023-11-10 16:47:40 +0530

    Update 3.py

--- OOPS/inheritance/singleInheritance/3.py
+++ OOPS/inheritance/singleInheritance/3.py
@@ -2,4 +2,29 @@
 # doctor_details with attributes : doc_name, specialization
 # patient with attributes : patient_name, token_number
 
-class doctor_details
+
+class Doctor:
+    def __init__(self, doc_name, specialization):
+        self.doc_name = doc_name
+        self.specialization = specialization
+
+
+class Patient(Doctor):
+    def __init__(self, doc_name, specialization, p_name, token):
+        super().__init__(doc_name, specialization)
+        self.p_name = p_name
+        self.token = token
+
+    def display(self):
+        print()
+        print(f"Doctor Name :{self.doc_name}")
+        print(f"Specialization :{self.specialization}")
+        print(f"Patient Name :{self.p_name}")
+        print(f"Token No. :{self.token}")
+
+
+obj = Patient("Dr. John Smith",
+              "Cardiology",
+              "Michael Jones",
+              "123")
+obj.display()

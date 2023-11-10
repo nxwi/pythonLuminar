056a25c951a4fd1579245c4467a68b4c4c028e58
Naswih <51323070+nxwi@users.noreply.github.com>
2023-11-10 16:06:18 +0530

    Create classSyntax.py

---
+++ OOPS/classAndObjects/classSyntax.py
@@ -0,0 +1,22 @@
+class Calc:
+    def add(self, a, b):
+        print(a + b)
+
+    def sub(self, a, b):
+        print(a - b)
+
+    def mult(self, a, b):
+        print(a * b)
+
+    def div(self, a, b):
+        print(a / b)
+
+    def rem(self, a, b):
+        print(a % b)
+
+
+Calc().add(1, 2)
+Calc().sub(5, 2)
+Calc().mult(2, 3)
+Calc().div(10, 5)
+Calc().rem(5, 2)
